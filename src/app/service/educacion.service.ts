2ebb1a48c44adc3c16d5f3d8bdcfe83f3dd3969b
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2024-04-07 01:59:17 -0300

    q

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class EducacionService {
 
-  URL = 'http://localhost:8080/educacion/';
+  URL = 'https://portfolioback-g3cj.onrender.com/educacion/';
 
   constructor(private httpClient: HttpClient) { }
 
